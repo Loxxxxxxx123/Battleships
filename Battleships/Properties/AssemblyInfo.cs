c82ae97486d7b80785fa3cec7f80cdf835cc064b
Private_OL1 <Private_OL1@DESKTOP-DAIA3GQ>
2025-04-16 18:53:24 +0200

    Add project files.

---
+++ Battleships/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Battleships")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Battleships")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fa80489d-5656-4403-8eb6-5a92b1884a44")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/GameSpecification.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using Battleships.Domain;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GameSpecification
+    {
+        [Test]
+        public void Constructor_ShouldCreateNotStarted()
+        {
+            var game = new Game();
+            game.Stage.Should().Be(GameStage.NotStarted);
+            game.FirstPlayer.Should().BeNull();
+            game.SecondPlayer.Should().BeNull();
+            game.CurrentPlayer.Should().BeNull();
+        }
+
+        [Test]
+        public void Game_ShouldPlay()
+        {
+            var game = new Game(options =>
+            {
+                options.SetShipSizes(3);
+                options.Width = 10;
+                options.Height = 10;
+            });
+
+            game.Start("first", "second");
+
+            game.Stage.Should().Be(GameStage.ArrangingShips);
+
+            var ship1 = game.CurrentPlayer.Field.GetShipToPutOrNull();
+            game.CurrentPlayer.Field.PutShip(ship1, new Point(2, 5));
+            game.EndArrangingCurrentPlayerShips();
+
+            game.Stage.Should().Be(GameStage.ArrangingShips);
+
+            var ship2 = game.CurrentPlayer.Field.GetShipToPutOrNull();
+            game.CurrentPlayer.Field.PutShip(ship2, new Point(2, 5));
+            game.EndArrangingCurrentPlayerShips();
+
+            game.Stage.Should().Be(GameStage.Battle);
+
+            game.ShootTo(new Point(2, 5));
+            game.ShootTo(new Point(3, 5));
+            game.ShootTo(new Point(4, 5));
+
+            game.Stage.Should().Be(GameStage.Finished);
+        }
+    }
+}
